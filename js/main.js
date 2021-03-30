74b980af4b453911ecfc5dbc4740e6269e1d8edb
Paweł Kubala <quolumbo@gmail.com>
2021-03-30 13:25:35 +0200

    chore: navigation functional

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
+const burger = document.querySelector(".burger");
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileNavItems = document.querySelectorAll(".mobile-nav__item");
 
-const burger = document.querySelector('.burger'); 
-console.log(burger); 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('burger--active'); 
-})
+const toggleMobileNav = () => {
+  burger.classList.toggle("burger--active");
+  mobileNav.classList.toggle("mobile-nav--closed");
+};
+
+burger.addEventListener("click", toggleMobileNav);
+
+for (const navItem of mobileNavItems) {
+  navItem.addEventListener("click", toggleMobileNav);
+}
